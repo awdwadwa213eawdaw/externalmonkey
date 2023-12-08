500d0515178a8cd735db7fe02d978cf5da838578
awdwadwa213eawdaw <142877519+awdwadwa213eawdaw@users.noreply.github.com>
2023-12-08 18:46:57 +0100

    Created CAFB9E49-0A7B-4CF0-B2CD-0E5DACA6EE61_MS.lua

---
+++ CAFB9E49-0A7B-4CF0-B2CD-0E5DACA6EE61_MS.lua
@@ -0,0 +1,63 @@
+local bgcTag = {
+	Text = "💎 | BGC",
+	Color = Color3.fromRGB(0, 0, 255),
+}
+
+return {
+	GroupId = 32407347,
+	TagById = {
+		["452198911"] = bgcTag,
+		["3768615414"] = bgcTag,
+		["1906095486"] = bgcTag,
+	},
+	GroupTags = {
+		["254"] = {
+			Text = "👑 | Founder",
+			Color = Color3.fromRGB(0, 0, 0),
+		},
+		["253"] = {
+			Text = "⭐ | Owner",
+			Color = Color3.fromRGB(212, 23, 225),
+		},
+		["252"] = {
+			Text = "🌟 | Co-Owner",
+			Color = Color3.fromRGB(199, 255, 222),
+		},
+		["251"] = {
+			Text = "🦀 | Head Of Staff",
+			Color =  Color3.fromRGB(255, 69, 94),
+		},
+		["250"] = {
+			Text = "🛡️ | lor HR",
+			Color = Color3.fromRGB(33, 23, 170),
+		},
+		["249"] = {
+			Text = "💬 | Community Manager",
+			Color = Color3.fromRGB(158, 179, 23),
+		},
+		["248"] = {
+			Text = "🚫 | Senior Administrator",
+			Color = Color3.fromRGB(158, 179, 23),
+		},
+		["247"] = {
+			Text = "🚫 | Administrator",
+			Color = Color3.fromRGB(91, 132, 101),
+		},
+		["246"] = {
+			Text = "🛠️ | Developer",
+			Color = Color3.fromRGB(119, 180, 255),
+		},
+		["245"] = {
+			Text = "✨ | Senior Moderator",
+			Color = Color3.fromRGB(255, 124, 64),
+		},
+		["244"] = {
+			Text = "✨ | Moderator",
+			Color = Color3.fromRGB(33, 111, 65),
+		},
+		["1"] = {
+			Text = "✅ | Verified",
+			Color3.fromRGB(225, 225, 225),
+		},
+	}
+}
